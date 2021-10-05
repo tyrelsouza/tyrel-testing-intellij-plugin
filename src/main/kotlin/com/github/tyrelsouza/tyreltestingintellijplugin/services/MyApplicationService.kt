7c53767f292c54f45ad6b6e27d6d36a2dc763a02
GitHub Action <action@github.com>
2021-10-05 19:43:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tyrelsouza/tyreltestingintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tyrelsouza.tyreltestingintellijplugin.services
+
+import com.github.tyrelsouza.tyreltestingintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/tyrelsouza/tyreltestingintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tyrelsouza/tyreltestingintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tyrelsouza.tyreltestingintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tyrelsouza.tyreltestingintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
